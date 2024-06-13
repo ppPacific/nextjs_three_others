9a5d1743ff645c177511a42420dcfcd592548154
tiffany.or <tiffany.or@pontacdigital.com>
2024-06-13 18:14:13 +0800

    update navbutton

---
+++ src/components/navigation/NavButton.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import {Home} from "lucide-react";
+const getIcon = (icon) => {
+    switch (icon) {
+        case "home":
+            return <Home className="w-full h-auto" strokeWidth={1.5} />;
+        // case "about":
+        //     return <User className="w-full h-auto" strokeWidth={1.5} />;
+        // case "projects":
+        //     return <Palette className="w-full h-auto" strokeWidth={1.5} />;
+        // case "contact":
+        //     return <Phone className="w-full h-auto" strokeWidth={1.5} />;
+        // case "github":
+        //     return <Github className="w-full h-auto" strokeWidth={1.5} />;
+        // case "linkedin":
+        //     return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
+        // case "twitter":
+        //     return <Twitter className="w-full h-auto" strokeWidth={1.5} />;
+        // case "resume":
+        //     return <NotebookText className="w-full h-auto" strokeWidth={1.5} />;
+
+        default:
+            return <Home className="w-full h-auto" strokeWidth={1.5} />;
+    }
+};
+
+const NavButton = ({
+    x,
+    y,
+    label,
+    link,
+    icon,
+    newTab}) => {
+
+    return (
+        <div className={"absolute cursor-pointer z-50"} style={{
+            transform: `translate(${x}, ${y})`
+        }}>
+            <Link href={link} target={newTab? '_blank':'_self'} className={'flex items-center justify-center text-foreground rounded-full'}
+            aria-label={label} name={label}>
+                <span className={"relative  w-14 h-14 p-4"}>
+                    {getIcon(icon)}
+                </span>
+            </Link>
+        </div>
+    )
+}
+export default NavButton
